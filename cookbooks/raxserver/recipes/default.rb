b9f2c1abcbeb094af2226867341536fa661fd206
Brandon Zitzman <bzitzman@answersingenesis.org>
2015-07-01 15:02:46 -0400

    first commit

---
+++ cookbooks/raxserver/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: raxserver
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'apt'
+include_recipe 'raxserver::hostfile'
---
+++ cookbooks/raxserver/recipes/hostfile.rb
@@ -0,0 +1,5 @@
+hostsfile_entry '172.16.4.222' do 
+  hostname 'git.aigdev.com'
+  action :create
+  comment 'Added by Chef'
+end
